165179f5101054c6c00d411bc47557d2983596de
Markus Rudy <webmaster@burgerdev.de>
2019-06-11 10:49:56 +0200

    make z_of_cstruct safe but inefficient

--- src/serde.ml
+++ src/serde.ml
@@ -1,18 +1,19 @@
 
-(* TODO DANGER: destructs the given buffer! *)
+(* TODO Allocates a new buffer and copies data. *)
 let z_of_cstruct num_bits buf =
   (* cut additional data *)
   let rem_bit = num_bits mod 8 in
   let num_bytes = if rem_bit > 0 then num_bits / 8 + 1 else num_bits / 8 in
-  let buf = Cstruct.sub buf 0 num_bytes in
+  let dstbuf = Cstruct.create num_bytes in
+  Cstruct.blit buf 0 dstbuf 0 num_bytes;
   let _ = if rem_bit > 0 then
       (* mask upper bits from byte that is consumed partially *)
-      Cstruct.get_uint8 buf (num_bytes - 1)
+      Cstruct.get_uint8 dstbuf (num_bytes - 1)
       |> (land) @@ 1 lsl rem_bit - 1
-      |> Cstruct.set_uint8 buf (num_bytes - 1)
+      |> Cstruct.set_uint8 dstbuf (num_bytes - 1)
   in
   (* read Z from LE encoded string *)
-  Cstruct.to_string buf |> Z.of_bits
+  Cstruct.to_string dstbuf |> Z.of_bits
 
 let bit_at buf i =
   let byte_index = i / 8 in
